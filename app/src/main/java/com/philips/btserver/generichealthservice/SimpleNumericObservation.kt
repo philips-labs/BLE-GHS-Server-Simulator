288d640c7783495413a974a210fa0e2f8ee876a3
Abdul Nabi <abdul.nabi@philips.com>
2021-03-05 12:27:31 +0100

    Remove observation deserialize methods since not used in peripheral (but in central) and adds unit test overhead.

--- app/src/main/java/com/philips/btserver/generichealthservice/SimpleNumericObservation.kt
+++ app/src/main/java/com/philips/btserver/generichealthservice/SimpleNumericObservation.kt
@@ -19,75 +19,7 @@ data class SimpleNumericObservation(override val id: Short,
         get() { return encodeTLV(valueCode, valueLength, value, valuePrecision) }
 
     companion object {
-
         internal const val valueCode = 0x00010A56
         internal const val valueLength = 4
-
-        fun deserialize(bytes: ByteArray): SimpleNumericObservation {
-            val parser = BluetoothBytesParser(bytes, 0, ByteOrder.BIG_ENDIAN)
-
-            // Parse type
-            val parsedTypeCode = parser.getIntValue(FORMAT_UINT32)
-            if (parsedTypeCode != typeCode) {
-                Timber.e("Expected typeCode but got %d", parsedTypeCode)
-            }
-            val parsedTypeLength = parser.getIntValue(FORMAT_UINT16)
-            if (parsedTypeLength != typeLength) {
-                Timber.e("Expected typeLength but got %d", parsedTypeLength)
-            }
-            val parsedType = ObservationType.fromValue(parser.getIntValue(FORMAT_UINT32)!!)
-            if (parsedType == ObservationType.UNKNOWN_STATUS_CODE) {
-                Timber.e("Unknown observation type")
-            }
-
-            // Parse id
-            val parsedHandleCode = parser.getIntValue(FORMAT_UINT32)
-            if (parsedHandleCode != handleCode) {
-                Timber.e("Expected handleCode but got %d", parsedHandleCode)
-            }
-            val parsedHandleLength = parser.getIntValue(FORMAT_UINT16)
-            if (parsedHandleLength != handleLength) {
-                Timber.e("Expected handleLength but got %d", parsedHandleLength)
-            }
-            val parsedId = parser.getIntValue(FORMAT_UINT16)!!
-
-            // Parse value
-            val parsedValueCode = parser.getIntValue(FORMAT_UINT32)
-            if (parsedValueCode != valueCode) {
-                Timber.e("Expected valueCode but got %d", parsedValueCode)
-            }
-            val parsedValueLength = parser.getIntValue(FORMAT_UINT16)
-            if (parsedValueLength != valueLength) {
-                Timber.e("Expected valueLength but got %d", parsedValueLength)
-            }
-            val parsedValue = parser.getFloatValue(BluetoothBytesParser.FORMAT_FLOAT, parser.offset, ByteOrder.BIG_ENDIAN)!!.toFloat()
-            val parsedValuePrecision = -(parser.getIntValue(FORMAT_UINT32)!! shr 24).toInt()
-
-            // Parse unit
-            val parsedUnitCode = parser.getIntValue(FORMAT_UINT32)
-            if (parsedUnitCode != unitCodeId) {
-                Timber.e("Expected unitCode but got %d", parsedUnitCode)
-            }
-            val parsedUnitLength = parser.getIntValue(FORMAT_UINT16)
-            if (parsedUnitLength != unitLength) {
-                Timber.e("Expected unitLength but got %d", parsedUnitLength)
-            }
-            val parsedUnit = UnitCode.fromValue(parser.getIntValue(FORMAT_UINT32)!!)
-            if (parsedUnit === UnitCode.UNKNOWN_CODE) {
-                Timber.e("Unknown unit type")
-            }
-
-            // Parse timestamp
-            val parsedTimestampCode = parser.getIntValue(FORMAT_UINT32)
-            if (parsedTimestampCode != timestampCode) {
-                Timber.e("Expected timestampCode but got %d", parsedTimestampCode)
-            }
-            val parsedTimestampLength = parser.getIntValue(FORMAT_UINT16)
-            if (parsedTimestampLength != timestampLength) {
-                Timber.e("Expected timestampLength but got %d", parsedTimestampLength)
-            }
-            val parsedTimestamp = Date(parser.longValue)
-            return SimpleNumericObservation(parsedId.toShort(), parsedType, parsedValue, parsedValuePrecision, parsedUnit, parsedTimestamp)
-        }
     }
 }
