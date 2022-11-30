b274121d73024f03842a4cd69c0b252dc418a43b
Abdul Nabi <abdul@codekrafters.com>
2022-11-30 18:52:17 +0100

    Minor cleanup to DIS

--- app/src/main/java/com/philips/btserver/gatt/DeviceInformationService.kt
+++ app/src/main/java/com/philips/btserver/gatt/DeviceInformationService.kt
@@ -10,24 +10,29 @@ import android.os.Build
 import com.welie.blessed.BluetoothPeripheralManager
 import com.philips.btserver.BaseService
 import com.philips.btserver.BluetoothServer
-import com.philips.btserver.userdataservice.UserDataManager
-import com.philips.btserver.userdataservice.UserDataService
+import com.philips.btserver.extensions.*
 import com.welie.blessed.BluetoothCentral
 import com.welie.blessed.GattStatus
 import com.welie.blessed.ReadResponse
-import java.nio.charset.Charset
 import java.util.*
+
+enum class UDIFlags(override val bit: Long) : Flags {
+    labelPresent((1 shl 0).toLong()),
+    deviceIdentifierPresent((1 shl 1).toLong()),
+    issuerPresent((1 shl 2).toLong()),
+    authorityPresent((1 shl 3).toLong())
+}
 
 internal class DeviceInformationService(peripheralManager: BluetoothPeripheralManager) : BaseService(peripheralManager) {
 
     override val service = BluetoothGattService(DIS_SERVICE_UUID, BluetoothGattService.SERVICE_TYPE_PRIMARY)
-    private val manufacturerChar = BluetoothGattCharacteristic(MANUFACTURER_NAME_CHARACTERISTIC_UUID, BluetoothGattCharacteristic.PROPERTY_READ, BluetoothGattCharacteristic.PERMISSION_READ)
-    private val modelNumberChar = BluetoothGattCharacteristic(MODEL_NUMBER_CHARACTERISTIC_UUID, BluetoothGattCharacteristic.PROPERTY_READ, BluetoothGattCharacteristic.PERMISSION_READ)
-    private val uniqueDeviceIdentifierChar = BluetoothGattCharacteristic(UDI_CHARACTERISTIC_UUID, BluetoothGattCharacteristic.PROPERTY_READ, BluetoothGattCharacteristic.PERMISSION_READ)
+    private val manufacturerChar = createReadCharacteristic(MANUFACTURER_NAME_CHARACTERISTIC_UUID)
+    private val modelNumberChar = createReadCharacteristic(MODEL_NUMBER_CHARACTERISTIC_UUID)
+    private val uniqueDeviceIdentifierChar = createReadCharacteristic(UDI_CHARACTERISTIC_UUID)
 
-    var manufacturer: String = Build.MANUFACTURER
-    var modelNumber: String = Build.MODEL
-    var udiValue: ByteArray = setUDI("{01}00844588003288{17}141120{10}7654321D{21}10987654d321", "00844588003288", "2.51", "2.16.840.1.113883.3.24")
+    val manufacturer = Build.MANUFACTURER
+    val modelNumber = Build.MODEL
+    val udiValue = udiBytes(UDI_LABEL, UDI_DEVICE_ID, UDI_ISSUER, UDI_AUTHORITY)
 
     override fun onCharacteristicRead(
         central: BluetoothCentral,
@@ -41,11 +46,49 @@ internal class DeviceInformationService(peripheralManager: BluetoothPeripheralMa
         }
     }
 
+    private fun createReadCharacteristic(charUUID: UUID): BluetoothGattCharacteristic {
+        return BluetoothGattCharacteristic(charUUID, BluetoothGattCharacteristic.PROPERTY_READ, BluetoothGattCharacteristic.PERMISSION_READ)
+    }
+
+    private fun udiBytes(label : String = "", deviceIdentifier : String = "", issuer : String = "", authority: String = "" ) : ByteArray {
+        var bytes = byteArrayOf()
+        var udiFlags = BitMask(0)
+
+        if (label.isNotEmpty()) {
+            udiFlags = udiFlags.set(UDIFlags.labelPresent)
+            bytes += udiBytesFor(label)
+        }
+        if (deviceIdentifier.isNotEmpty())  {
+            udiFlags = udiFlags.set(UDIFlags.deviceIdentifierPresent)
+            bytes += udiBytesFor(deviceIdentifier)
+        }
+        if (issuer.isNotEmpty()) {
+            udiFlags = udiFlags.set(UDIFlags.issuerPresent)
+            bytes += udiBytesFor(issuer)
+        }
+        if (authority.isNotEmpty()) {
+            udiFlags = udiFlags.set(UDIFlags.authorityPresent)
+            bytes += udiBytesFor(authority)
+        }
+
+        bytes = byteArrayOf(udiFlags.value.toByte()) + bytes
+        return bytes
+    }
+
+    private fun udiBytesFor(udiString: String): ByteArray {
+        return  udiString.toByteArray(Charsets.UTF_8) + 0x0.toByte()
+    }
+
     companion object {
         val DIS_SERVICE_UUID = UUID.fromString("0000180A-0000-1000-8000-00805f9b34fb")
         val MANUFACTURER_NAME_CHARACTERISTIC_UUID = UUID.fromString("00002A29-0000-1000-8000-00805f9b34fb")
         val MODEL_NUMBER_CHARACTERISTIC_UUID = UUID.fromString("00002A24-0000-1000-8000-00805f9b34fb")
         val UDI_CHARACTERISTIC_UUID = UUID.fromString("00007F3A-0000-1000-8000-00805f9b34fb")
+
+        const val UDI_LABEL = "{01}00844588003288{17}141120{10}7654321D{21}10987654d321"
+        const val UDI_DEVICE_ID = "00844588003288"
+        const val UDI_ISSUER = "2.51"
+        const val UDI_AUTHORITY = "2.16.840.1.113883.3.24"
 
         // If the BluetoothService has a running DeviceInformationService then return it
         fun getInstance(): DeviceInformationService? {
@@ -60,36 +103,5 @@ internal class DeviceInformationService(peripheralManager: BluetoothPeripheralMa
         service.addCharacteristic(modelNumberChar)
         service.addCharacteristic(uniqueDeviceIdentifierChar)
     }
-
-    private fun  setUDI(UDILabel : String = "", DeviceIdentifier : String = "", UDI_Issuer : String = "", UDI_Authority: String = "" ) : ByteArray {
-        var bytes = byteArrayOf()
-        var flags = 0x0
-        val UDI_Label_present = 0x01
-        val UDI_Device_Identifier_present = 0x02
-        val UDI_Issuer_present = 0x04
-        val UDI_Authority_present = 0x08
-
-
-        if (UDILabel.length > 0) {
-            flags.or( UDI_Label_present)
-            bytes = UDILabel.toByteArray(Charsets.UTF_8) + 0x0.toByte()
-        }
-        if (DeviceIdentifier.length > 0)  {
-            flags.or(UDI_Device_Identifier_present)
-            bytes = bytes + DeviceIdentifier.toByteArray(Charsets.UTF_8) + 0x0.toByte()
-        }
-        if (UDI_Issuer.length > 0) {
-            flags.or(UDI_Issuer_present)
-            bytes = bytes + UDI_Issuer.toByteArray(Charsets.UTF_8) + 0x0.toByte()
-        }
-        if (UDI_Authority.length > 0) {
-            flags.or(UDI_Authority_present)
-            bytes = bytes + UDI_Authority.toByteArray(Charsets.UTF_8) + 0x0.toByte()
-        }
-
-        bytes = byteArrayOf( flags.toByte()) + bytes
-        return bytes
-    }
-
 
 }
